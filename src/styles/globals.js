035d51def3179b8d324346a92f7a6ed70a5a8986
Sliver02 <japo02@hotmail.it>
2022-09-16 00:26:03 +0200

    global styles

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -1,5 +1,30 @@
 import { createGlobalStyle, css } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+    // global reset
+    *,
+    *:after,
+    *:before {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        vertical-align: baseline;
+        box-sizing: border-box;
+    }
 
+    // base global style
+    html, 
+    body {
+        font-display: swap;
+        scroll-behavior: smooth;
+        height: -webkit-fill-available;
+
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+
+        line-height: 1.3;
+    }
 `;
---
+++ src/styles/variables.js
@@ -0,0 +1,8 @@
+export const breakpoints = {
+    xs: 0,
+    sm: 458,
+    md: 769,
+    lg: 1025,
+    xl: 1441,
+    xxl: 1921,
+};
