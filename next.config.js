a443595c050cb2ebaa560ee7c1620603c12abd42
Sliver02 <japo02@hotmail.it>
2022-09-16 00:02:53 +0200

    next set up

--- next.config.js
+++ next.config.js
@@ -1,7 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+/**
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = nextConfig
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+// const withSass = require('@zeit/next-sass');
+
+const env = {};
+
+Object.keys(process.env).forEach((key) => {
+    env[key] = process.env[key];
+});
+
+// module.exports = withSass({
+//     cssModules: true,
+//     env,
+//     cssLoaderOptions: {
+//         url: false,
+//     },
+// });
+
+const nextConfig = {
+    webpack5: true,
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
+
+        return config;
+    },
+    images: {
+        domains: ['picsum.photos'],
+    },
+};
+
+module.exports = withPlugins([[withImages]], nextConfig);
---
+++ src/styles/globals.js
@@ -0,0 +1,5 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+
+`;
